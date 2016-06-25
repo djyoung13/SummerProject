cbb6b2581983a19e2ef260f1660bf53c79115e7c
djyoung13 <djyoung1@memphis.edu>
2016-06-25 17:05:14 -0500

    Port numbers fixed

--- src/endpoint/RPTracksPublisher.java
+++ src/endpoint/RPTracksPublisher.java
@@ -6,6 +6,6 @@ import ws.RPTracksImpl;;
 public class RPTracksPublisher {
 	public static void main(String[] args)
 	{
-		Endpoint.publish("http://localhost:5003/ws/RPTracks", new RPTracksImpl());
+		Endpoint.publish("http://localhost:8002/ws/RPTracks", new RPTracksImpl());
 	}
 }
